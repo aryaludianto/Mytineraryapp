12cf6dcaa2098996c89bc42a8733e03c579534fa
Arya Ludianto <45729368+aryaludianto@users.noreply.github.com>
2019-09-17 14:08:46 +0200

    Update Servertest

--- servTest.js
+++ servTest.js
@@ -40,15 +40,3 @@ app.use('/app', router);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
-
-
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://arya123:arya123@clustermytinerary-wulcn.gcp.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("mytinerary").collection("cities");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
