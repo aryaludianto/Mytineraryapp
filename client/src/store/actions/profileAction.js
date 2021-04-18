d261636df0e197de9bf584564c2b9f5f90ba7612
arya <arya.pratamal@ymail.com>
2021-04-18 17:42:31 +0200

    removed prod.json

--- client/src/store/actions/profileAction.js
+++ client/src/store/actions/profileAction.js
@@ -12,14 +12,14 @@ export const getProfile = () => dispatch => {
   axios
     .post('/profile/profiles', { emailOfUser: emailOfUser }, config)
     .then(res => {
-      console.log('get profile success')
+      // console.log('get profile success')
       dispatch({
         type: GET_PROFILE,
         payload: res.data
       });
     })
     .catch(err => {
-      console.log('error from get profile');
+      // console.log('error from get profile');
       console.log(err);
     });
 };
