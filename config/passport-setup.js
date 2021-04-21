999c85100e8d50c589c99e661fbd18d315a70fa4
arya <arya.pratamal@ymail.com>
2021-04-21 12:36:50 +0200

    added https

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,45 +3,8 @@ const Users = require('../models/users')
 const GooglePlusTokenStrategy = require("passport-google-plus-token");
 const config = require('./config');
 
-// const GoogleStrategy = require("passport-google-oauth20");
-
-// passport.use(
-//   "googleToken",
-//   new GooglePlusTokenStrategy({
-//     clientID: config.google.clientID,
-//     clientSecret: config.google.clientSecret,
-//     callbackURL: '/auth/google/redirect'
-//   }, (accessToken, refreshToken, profile, done) => {
-
-//     // console.log("this is profile from google", profile);
-//     Users.findOne({ email: profile.emails[0].value }).then(currentUser => {
-//       if (currentUser) {
-//         // console.log("user is", currentUser);
-//         let user = currentUser;
-//         done(null, user);
-//       } else {
-//         const profilePhoto = profile.photos[0].value;
-//         const profilePhotoEnlarged = profilePhoto.replace("sz=50", "sz=150");
-
-//         new Users({
-//           profilePhoto: profilePhotoEnlarged,
-//           username: profile.displayName,
-//           firstName: profile.name.givenName,
-//           lastName: profile.name.familyName,
-//           email: profile.emails[0].value
-//         })
-//           .save()
-//           .then(newAccount => {
-//             // console.log("new user created:", + newAccount);
-//             let user = newAccount;
-//             done(null, user);
-//           });
-//       }
-//     });
-//   })
-// )
-
 const absoluteUri = 'https://grim-monster-25961.herokuapp.com'
+
 passport.use(
   "googleToken",
   new GooglePlusTokenStrategy({
@@ -70,7 +33,6 @@ passport.use(
         })
           .save()
           .then(newAccount => {
-            // console.log("new user created:", + newAccount);
             let user = newAccount;
             done(null, user);
           });
